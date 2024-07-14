70558b2bb5839318c0c4d0cd59251a2845a7f56b
PavanUmmagani07 <pavankalyanU@outlook.com>
2024-07-14 11:09:06 +0530

    contacts us final

--- contactus.js
+++ contactus.js
@@ -102,51 +102,60 @@ function validateFormData(formData) {
     let {firstName, lastName, email, mobileNumber, comments} = formData;
     if (firstName === "") {
       firstNameErrorMsgEl.textContent = "Required*";
+        firstNameErrorMsgEl.style.color = 'Red';
+        firstNameErrorMsgEl.style.fontWeight = 'Bold';
     }
     if (lastName === "") {
         lastNameErrorMsgEl.textContent = "Required*";
+        lastNameErrorMsgEl.style.color = 'Red';
+        lastNameErrorMsgEl.style.fontWeight = 'Bold';
     } 
     if (email === ""){
         emailErrorMsgEl.textContent = "Required*";
+        emailErrorMsgEl.style.color = 'Red';
+        emailErrorMsgEl.style.fontWeight = 'Bold';
     }
     if (mobileNumber === ""){
         mobileNumberErrorMsgEl.textContent = "Required*";
+        mobileNumberErrorMsgEl.style.color = 'Red';
+        mobileNumberErrorMsgEl.style.fontWeight = 'Bold';
     }
     if (comments === ""){
         commentsErrorMsgEl.textContent = "Required*";
+        commentsErrorMsgEl.style.color = 'Red';
+        commentsErrorMsgEl.style.fontWeight = 'Bold';
     }
   }
 
 
-
-let checkboxEl = document.getElementById('checkbox');
-let checkboxErrorMsgEl = document.getElementById("checkboxErrorMsg");
-
 function submitFormData(){
+    let checkboxEl = document.getElementById('checkbox_id');
+    let checkboxErrorMsgEl = document.getElementById("checkboxErrorMsg");
     if(checkboxEl.checked===true){
         let options={
-            method:'POST',
+            method:"POST",
             headers:{
-                'Content-Type':'Application/json',
-                Accept:'Application/json',
+                "Content-Type":"application/json",
+                Accept:"application/json"
             },
             body:JSON.stringify(formData)
-        }
-        let url = 'http://localhost:3000/contactus';
+        };
+        let url = "http://localhost:3000/contactus";
         fetch(url,options)
-    
         .then(function(response){
             return response.json()
         })
         .then(function(jsonData){
             console.log(jsonData);
         })
-    }
-    else{
-        checkboxErrorMsgEl.textContent = 'Tick the Checkbox and Other Feilds before Submitting';
-        checkboxErrorMsgEl.style.color="Red"
+
+    }else{
+        checkboxErrorMsgEl.textContent="Tick the Checkbox to submit the data";
+        checkboxErrorMsgEl.style.color = 'Red';
+        checkboxErrorMsgEl.style.fontWeight='bold';
     }
 }
+
 
 let contactUsFormEl = document.getElementById("contactUsForm");
 contactUsFormEl.addEventListener('submit',function(event){
@@ -154,4 +163,110 @@ contactUsFormEl.addEventListener('submit',function(event){
     //console.log('Defalut Prevented')
     validateFormData(formData)
     submitFormData(formData)
+})
+
+
+//LOCATIONS
+let northAmericaEl = document.getElementById('northAmerica');
+let americaOfficesEl = document.getElementById('americaOffices');
+
+let europeEl = document.getElementById('europe');
+let europeOfficesEl = document.getElementById('europeOffices');
+
+let asiaEl = document.getElementById('asia');
+let asiaOfficesEl = document.getElementById('asiaOffices');
+
+let middleEastEl = document.getElementById('middleEast');
+let middleEastOfficesEl = document.getElementById('middleEastOffices');
+
+europeEl.addEventListener('click',function(){
+    if(europeOfficesEl.hasAttribute('hidden')){
+        europeOfficesEl.removeAttribute('hidden');
+        europeEl.classList.add('location-img-card');
+        
+    }
+
+    if(!americaOfficesEl.hasAttribute('hidden')){
+        americaOfficesEl.setAttributeNode(document.createAttribute("hidden"));
+        northAmericaEl.classList.remove('location-img-card')
+    }
+
+    if(!asiaOfficesEl.hasAttribute('hidden')){
+        asiaOfficesEl.setAttributeNode(document.createAttribute("hidden"));
+        asiaEl.classList.remove('location-img-card')
+    }
+
+    if(!middleEastOfficesEl.hasAttribute('hidden')){
+        middleEastOfficesEl.setAttributeNode(document.createAttribute("hidden"));
+        middleEastEl.classList.remove('location-img-card')
+    }
+
+})
+
+
+
+asiaEl.addEventListener('click',function(){
+    if(asiaOfficesEl.hasAttribute('hidden')){
+        asiaOfficesEl.removeAttribute('hidden')
+        asiaEl.classList.add('location-img-card')
+    }
+
+    if(!americaOfficesEl.hasAttribute('hidden')){
+        americaOfficesEl.setAttributeNode(document.createAttribute("hidden"));
+        northAmericaEl.classList.remove('location-img-card')
+    }
+
+    if(!europeOfficesEl.hasAttribute('hidden')){
+        europeOfficesEl.setAttributeNode(document.createAttribute("hidden"));
+        europeEl.classList.remove('location-img-card')
+    }
+
+    if(!middleEastOfficesEl.hasAttribute('hidden')){
+        middleEastOfficesEl.setAttributeNode(document.createAttribute("hidden"));
+        middleEastEl.classList.remove('location-img-card')
+    }
+})
+
+middleEastEl.addEventListener('click',function(){
+    if(middleEastOfficesEl.hasAttribute('hidden')){
+        middleEastOfficesEl.removeAttribute('hidden')
+        middleEastEl.classList.add('location-img-card')
+    }
+
+    if(!americaOfficesEl.hasAttribute('hidden')){
+        americaOfficesEl.setAttributeNode(document.createAttribute("hidden"));
+        northAmericaEl.classList.remove('location-img-card')
+    }
+
+    if(!europeOfficesEl.hasAttribute('hidden')){
+        europeOfficesEl.setAttributeNode(document.createAttribute("hidden"));
+        europeEl.classList.remove('location-img-card')
+    }
+
+    if(!asiaOfficesEl.hasAttribute('hidden')){
+        asiaOfficesEl.setAttributeNode(document.createAttribute("hidden"));
+        asiaEl.classList.remove('location-img-card')
+    }
+})
+
+northAmericaEl.addEventListener('click',function(){
+    if(americaOfficesEl.hasAttribute('hidden')){
+        americaOfficesEl.removeAttribute('hidden')
+        northAmericaEl.classList.add('location-img-card')
+    }
+
+    if(!middleEastOfficesEl.hasAttribute('hidden')){
+        middleEastOfficesEl.setAttributeNode(document.createAttribute("hidden"));
+        middleEastEl.classList.remove('location-img-card')
+    }
+
+    if(!europeOfficesEl.hasAttribute('hidden')){
+        europeOfficesEl.setAttributeNode(document.createAttribute("hidden"));
+        europeEl.classList.remove('location-img-card')
+    }
+
+    if(!asiaOfficesEl.hasAttribute('hidden')){
+        asiaOfficesEl.setAttributeNode(document.createAttribute("hidden"));
+        asiaEl.classList.remove('location-img-card')
+    }
 })
